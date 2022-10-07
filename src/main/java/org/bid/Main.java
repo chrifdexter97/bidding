be645253982fa947e2e85175d56264d3e6f57779
chrif dexter97 <chrifdexter97@gmail.com>
2022-10-07 11:21:22 +0200

    sonar

--- src/main/java/org/bid/Main.java
+++ src/main/java/org/bid/Main.java
@@ -2,17 +2,22 @@ package org.bid;
 
 import org.bid.presentation.CommandLineInteractor;
 
+import java.text.DateFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+//        Scanner in = new Scanner(System.in);
+//
+//        CommandLineInteractor commandLineInteractor = CommandLineInteractor.getCommandLineInteractor();
+//
+//        commandLineInteractor.start(in);
 
-        CommandLineInteractor commandLineInteractor = CommandLineInteractor.getCommandLineInteractor();
-
-        commandLineInteractor.start(in);
+        System.out.println(Locale.getDefault());
+        System.out.println(DateFormat.getDateInstance(DateFormat.SHORT,Locale.getDefault()));
 
     }
 
